c902117036bf7b0ab5549260b92f3eed1c6a15e7
odf <olaf.delgado@gmail.com>
2011-10-29 13:46:48 +1100

    Added graph methods with-vertices and with-edges

--- src/clojure_experiments/core.clj
+++ src/clojure_experiments/core.clj
@@ -1,7 +1,7 @@
 (ns clojure-experiments.core)
 
 (defprotocol IGraph
-  "A directed graph, not necessarily finite or locally finite."
+  "A directed graph, not necessarily, simple, finite or locally finite."
   (vertices [G])
   (pred [G])
   (succ [G])
@@ -24,8 +24,12 @@
                 (conj (pred G) [v (no-neighbors G)])
                 (conj (succ G) [v (no-neighbors G)]))))
 
+(defn with-vertices [G verts] (reduce with-vertex G verts))
+
 (defn with-edge [G e]
-  (let [[v w] e G1 (reduce with-vertex G e)]
+  (let [[v w] e G1 (with-vertices G e)]
     (Graph. (reduce conj (vertices G1) e)
             (conj (pred G1) [w (conj ((pred G1) w) v)])
             (conj (succ G1) [v (conj ((succ G1) v) w)]))))
+
+(defn with-edges [G edges] (reduce with-edge G edges))
