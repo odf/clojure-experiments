4a4adc494695b8522b7f0dc20c0be21ece0fb618
odf <olaf.delgado@gmail.com>
2011-10-29 17:46:31 +1100

    Fiddling...

--- src/clojure_experiments/core.clj
+++ src/clojure_experiments/core.clj
@@ -1,18 +1,24 @@
 (ns clojure-experiments.core)
 
 (defprotocol IGraph
-  "A directed graph, not necessarily, simple, finite or locally finite."
+  "A simple directed graph, not necessarily finite or locally finite."
   (vertices [G])
   (pred [G])
-  (succ [G])
-  (no-neighbors [G]))
+  (succ [G]))
 
 (defrecord Graph [verts back forw]
   IGraph
   (vertices [G] verts)
   (pred [G] back)
-  (succ [G] forw)
-  (no-neighbors [G] #{}))
+  (succ [G] forw))
+
+(defn edges [G]
+  (mapcat (fn [[v adj]] (map (partial vector v) adj))
+          (succ G)))
+
+(defn isolated? [G v] (and ((vertices G) v)
+                           (empty? ((pred G) v))
+                           (empty? ((succ G) v))))
 
 (def empty-graph (Graph. #{} {} {}))
 
@@ -21,15 +27,34 @@
         G
         true
         (Graph. (conj (vertices G) v)
-                (conj (pred G) [v (no-neighbors G)])
-                (conj (succ G) [v (no-neighbors G)]))))
+                (conj (pred G) [v #{}])
+                (conj (succ G) [v #{}]))))
 
 (defn with-vertices [G verts] (reduce with-vertex G verts))
 
-(defn with-edge [G e]
-  (let [[v w] e G1 (with-vertices G e)]
-    (Graph. (reduce conj (vertices G1) e)
+(defn with-edge [G [v w]]
+  (let [G1 (with-vertices G [v w])]
+    (Graph. (vertices G1)
             (conj (pred G1) [w (conj ((pred G1) w) v)])
             (conj (succ G1) [v (conj ((succ G1) v) w)]))))
 
 (defn with-edges [G edges] (reduce with-edge G edges))
+
+(defn without-edge [G [v w]]
+  (Graph. (vertices G)
+          (conj (pred G) [w (disj ((pred G) w) v)])
+          (conj (succ G) [v (disj ((succ G) v) w)])))
+
+(defn without-edges [G edges] (reduce without-edge G edges))
+
+(defn without-vertex [G v]
+  (cond (isolated? G v)
+        (Graph. (disj (vertices G) v)
+                (pred G)
+                (succ G))
+        ((vertices G) v)
+        (without-vertex
+         (without-edges
+          G (filter (fn ))))
+        ))
+
