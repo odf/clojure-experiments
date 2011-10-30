32ae070407c271045ff7b875aee9754c03ecdf1e
odf <olaf.delgado@gmail.com>
2011-10-30 17:38:30 +1100

    Work on inquiry methods.

--- src/clojure_experiments/core.clj
+++ src/clojure_experiments/core.clj
@@ -20,26 +20,37 @@
   (pred [G] back)
   (succ [G] forw))
 
+
 (defn graph [& vs] (apply with-edges (cons (Graph. #{} {} {}) vs)))
+
+(defn vertex? [G v] (and ((vertices G) v) true))
+
+(defn source? [G v] (and (vertex? G v) (empty? ((pred G) v)) true))
+
+(defn sink? [G v] (and (vertex? G v) (empty? ((succ G) v)) true))
+
+(defn internal? [G v] (not (or (empty? ((succ G) v)) (empty? ((pred G) v)))))
+
+(defn isolated? [G v] (and (vertex? G v) (empty? ((pred G) v)) (empty? ((succ G) v))))
+
+(defn edge? [G v w]
+  (and (vertex? G v)
+       (((succ G) v) w)
+       true))
 
 (defn edges [G]
   (mapcat (fn [[v adj]] (map vector (repeat v) adj))
           (succ G)))
 
-(defn isolated? [G v]
-  (and ((vertices G) v)
-       (empty? ((pred G) v))
-       (empty? ((succ G) v))))
-
-(defn has-edge? [G v w]
-  (and ((vertices G) v)
-       (((succ G) v) w)))
+(defn adj [G v]
+  "The successors of a vertex followed by the predecessors"
+  (concat ((succ G) v) ((pred G) v)))
 
 (defn with-vertices [G & vs]
   (when G
     (reduce
      (fn [G v]
-       (if ((vertices G) v)
+       (if (vertex? G v)
          G
          (Graph. (conj (vertices G) v)
                  (conj (pred G) [v #{}])
@@ -50,7 +61,7 @@
   (when G
     (reduce
      (fn [G v]
-       (if ((vertices G) v)
+       (if (vertex? G v)
         (let [purge-v-from-map (partial map-values (partial filter-set #(not= % v)))]
           (Graph. (disj (vertices G) v)
                   (dissoc (purge-v-from-map (pred G)) v)
@@ -62,7 +73,7 @@
   (when G
     (reduce
      (fn [G [v w]]
-       (if (or (has-edge? G v w) (= v w))
+       (if (or (edge? G v w) (= v w))
          G
          (let [G1 (with-vertices G v w)]
            (Graph. (vertices G1)
@@ -74,7 +85,7 @@
   (when G
     (reduce
      (fn [G [v w]]
-       (if (has-edge? G v w)
+       (if (edge? G v w)
          (Graph. (vertices G)
                  (conj (pred G) [w (disj ((pred G) w) v)])
                  (conj (succ G) [v (disj ((succ G) v) w)]))
