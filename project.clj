b927481835ce58929fcb09002effa0d131e7e57d
odf <olaf.delgado@gmail.com>
2013-01-17 17:03:12 +1100

    Added criterium to the dependencies list

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clojure-experiments "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [criterium "0.2.1"]]
   :java-source-path "src/java")
